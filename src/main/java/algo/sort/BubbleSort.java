0831aad602411ae65a19606977ad7cc341f32977
Janosch Moor <janoschmoor@Janoschs-MacBook-Air.local>
2023-12-23 19:52:21 +0100

    BubbleSort comments

--- src/main/java/algo/sort/BubbleSort.java
+++ src/main/java/algo/sort/BubbleSort.java
@@ -4,17 +4,20 @@ public class BubbleSort {
     
     public int[] sort (int[] A) {
 
-        
+        // loop
         for (int i = 0; i < A.length; i++) {
             for (int j = 1; j < A.length; j++) {
+
+                // swap if pair is unordered
                 if (A[j-1] > A[j]) {
                     int temp = A[j];
                     A[j] = A[j-1];
                     A[j-1] = temp;
                 }
+                
             }
         }
+
         return A;
     }
-
 }
